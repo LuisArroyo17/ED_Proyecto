d89f47a760ea7477ea21bb4c3d7ab399d8ace6f4
Luis Yazid Arroyo Vasquez <135352670+LuisArroyo17@users.noreply.github.com>
2024-09-03 00:10:38 -0500

    wa

--- src/controllers/admin.py
+++ src/controllers/admin.py
@@ -4,10 +4,11 @@ from flask import request
 class AdminController:
     @staticmethod
     def crear_admin():
-        username = request.json.get('username: ')
-        password = request.json.get('password:')
-        first_name = request.json.get('first_name:')
-        last_name = request.json.get('last_name:')
-        response= AdminModel().insertar_admin(username, password, first_name, last_name)
+        username = request.json.get('username')
+        password = request.json.get('password')
+        first_name = request.json.get('first_name')
+        last_name = request.json.get('last_name')
+        response= AdminModel().insertar_adminDB(username, password, first_name, last_name)
         if response:
-            print('Admin creado con éxito')
+            return response
+        return { "error": "No se pudo registrar" }, 400
--- src/models/admin.py
+++ src/models/admin.py
@@ -1,22 +1,37 @@
 from utils.conectarDB import DB
 
+
 class AdminModel:
-    def __init__(self) :
-        self.db = DB.connection()
-        
-    def __del__(self):
+    def __init__(self) -> None:
+        self.db = DB().connection()
+
+    def __del__(self) -> None:
         if self.db:
             self.db.close()
-        
-    def insertar_admin(self, username, password, first_name, last_name):
+
+    def insertar_adminDB(self, username, password, first_name, last_name):
         cursor = self.db.cursor()
         try:
             cursor.execute("""
-                insert into admin (username, password, first_name, last_name)
-                values (%s, %s, %s, %s)
+                INSERT INTO admin (username, password, first_name, last_name)
+                VALUES (%s, %s, %s, %s)
             """, (username, password, first_name, last_name))
             self.db.commit()
-            cursor.close()
-            return True
+            return { "last_row_id": cursor.lastrowid, "row_count": cursor.rowcount }, 201
         except Exception as e:
             print(f"Error: {e}")
+            # Devuelve una respuesta en caso de error
+            return { "status": "error", "message": "No se pudo insertar el admin", "error": str(e) }, 500
+
+
+
+
+# class AdminModel:
+#     def __init__(self) :
+#         self.db = DB.connection()
+        
+#     def __del__(self):
+#         if self.db:
+#             self.db.close()
+        
+
