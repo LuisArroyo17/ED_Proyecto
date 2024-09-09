b8a3b0d18a4a7ee2bb04c1c599ca855363dc092f
Luis Yazid Arroyo Vasquez <135352670+LuisArroyo17@users.noreply.github.com>
2024-09-08 21:35:48 -0500

    wa

--- src/controllers/productos.py
+++ src/controllers/productos.py
@@ -23,6 +23,7 @@ class ProductosController:
         
         producto=ModelProducto().agregar_productoDB(nombre, precio, categoria, descripcion)
         return producto
+    
     @staticmethod
     def eliminar_producto(self, nombre):
         producto = self.buscar_producto(nombre)
@@ -30,6 +31,7 @@ class ProductosController:
             self.productos.remove(producto)
             return True
         return False
+    
     @staticmethod
     def actualizar_producto(self, id):
         producto = self.obtener_producto(id)
