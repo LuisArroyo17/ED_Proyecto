09cd9ef5ce5420a1004c3e5b4f3f577b63998024
LuisArroyo17 <luis.arroyo3@unmsm.edu.pe>
2024-10-29 19:02:33 -0500

    Reapply "Merge remote-tracking branch 'origin/RamaF' into ramaY"
    This reverts commit 015648eadd3cddc7353d814f015df8b88ba70db1.

--- src/run.py
+++ src/run.py
@@ -1,11 +1,6 @@
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from flask import Flask
-from routes import productos_blueprint, pedidos_blueprint, usuarios_blueprint, envios_blueprint
+from routes import productos_blueprint, pedidos_blueprint, usuarios_blueprint, envios_blueprint, arbol_blueprint
 from flask_cors import CORS
-from config import Config
 
 app = Flask(__name__)
 CORS(app)
@@ -16,10 +11,7 @@ app.register_blueprint(pedidos_blueprint)
 app.register_blueprint(productos_blueprint)
 app.register_blueprint(usuarios_blueprint)
 app.register_blueprint(envios_blueprint)  # Registrar el blueprint de envíos
+app.register_blueprint(arbol_blueprint)
 
 if __name__ == "__main__":
   app.run(debug=True)
-
-@app.route('/')
-def home():
-  return "Servidor funcionando correctamente"
